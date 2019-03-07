898590c485ed44587dca0087633bbf2f0d5c2942
paramesh33 <33004813+paramesh33@users.noreply.github.com>
2019-03-07 10:31:10 +0530

    Create special.py

---
+++ special.py
@@ -0,0 +1,10 @@
+string = input();
+alphabets = digits = special = 0
+for i in range(len(string)):
+    if(string[i].isalpha()):
+        alphabets = alphabets + 1
+    elif(string[i].isdigit()):
+        digits = digits + 1
+    else:
+        special = special + 1
+print(special)
