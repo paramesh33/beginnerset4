177133ad4f193809c14cc90ac5585dd07ef1c976
paramesh33 <33004813+paramesh33@users.noreply.github.com>
2019-03-07 10:59:45 +0530

    Update powerof2.py

--- powerof2.py
+++ powerof2.py
@@ -1,12 +1,6 @@
 import math 
 a=input();
-def Log2(x): 
-    return (math.log10(x) / 
-            math.log10(2));
-def isPowerOfTwo(n): 
-    return (math.ceil(Log2(n)) == 
-            math.floor(Log2(n))); 
-if(isPowerOfTwo(a)): 
-    print("Yes"); 
-else: 
-    print("No"); 
+if ( a & (a - 1)):
+    print ("no");
+else:
+    print ("yes")
