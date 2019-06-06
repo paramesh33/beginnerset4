b016ecb5f1d57f4093c7702b02e0f9af9fb87904
paramesh33 <33004813+paramesh33@users.noreply.github.com>
2019-06-06 09:15:23 +0530

    Create printstr.py

---
+++ printstr.py
@@ -0,0 +1,21 @@
+a=int(input())
+if a==1:
+    print('One')
+if a==2:
+    print('Two')
+if a==3:
+    print('Three')
+if a==4:
+    print('Four')
+if a==5:
+    print('Five')
+if a==6:
+    print('Six')
+if a==7:
+    print('Seven')
+if a==8:
+    print('Eight')
+if a==9:
+    print('Nine')
+if a==10:
+    print('Ten')
